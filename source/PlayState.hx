ec460191454483919797e9835c3af9b87c435bff
taoaustin <taoaustin@yahoo.ca>
2021-09-19 13:44:41 -0400

    Notes fade out at consistent positions, accounting for different song speeds

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1657,7 +1657,7 @@ class PlayState extends MusicBeatState
 				nextUnspawnedNote++;
 
 				var index:Int = unspawnNotes.indexOf(dunceNote);    // this part was kinda pointless even in the old implementation since the index would always be 0, 
-				trace(index);                                       // 	this is because unspawnNotes would be sorted according to strumTime in the first place --austin
+				//trace(index);                                       // 	this is because unspawnNotes would be sorted according to strumTime in the first place --austin
 				//trace(dunceNote.alive);
 				// unspawnNotes.splice(index, 1);
 			}
@@ -1679,9 +1679,13 @@ class PlayState extends MusicBeatState
 				}
 
 				daNote.y = (strumLine.y - (Conductor.songPosition - daNote.strumTime) * (0.45 * FlxMath.roundDecimal(SONG.speed, 2)));
-				if (daNote.visible && faded)
+				if (daNote.visible && (daNote.y <= (FlxG.width / 2)) && faded)
 				{
-					daNote.alpha -= 0.02;
+					// x_0 = strumLine.y + 50 (~100), y_0 = 0      x_1 = FlxG.width / 2, y_1 = 1
+					// I want opacity to go from 1 to 0 from about halfway up the screen, to 50px before the strumLine, subject to change/tweaks
+					var slope:Float = 1 / ((FlxG.width / 2) - (strumLine.y + 50));   // slope
+					daNote.alpha = slope * (daNote.y - (strumLine.y)) + 0;           // just a linear function in the form y = m(x - x_0) + y_0
+
 				}
 
 				// i am so fucking sorry for this if condition
