266186d5c424e8a51a34e2a207ffe9ff34a9dafc
taoaustin <taoaustin@yahoo.ca>
2021-09-18 21:43:09 -0400

    Added comments  & test json song formatting

--- source/Conductor.hx
+++ source/Conductor.hx
@@ -17,8 +17,8 @@ typedef BPMChangeEvent =
 class Conductor
 {
 	public static var bpm:Int = 100;
-	public static var crochet:Float = ((60 / bpm) * 1000); // beats in milliseconds
-	public static var stepCrochet:Float = crochet / 4; // steps in milliseconds
+	public static var crochet:Float = ((60 / bpm) * 1000); // MILLISECONDS PER BEAT (ex: if bpm=180, then 1 beat=333.33 ms)    beats in milliseconds
+	public static var stepCrochet:Float = crochet / 4;     // MILLISECONDS PER STEP/POSSIBLE NOTE? (assuming 4/4 time)             steps in milliseconds
 	public static var songPosition:Float;
 	public static var lastSongPos:Float;
 	public static var offset:Float = 0;
